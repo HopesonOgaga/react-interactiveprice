11c334279965bb0a595df77b0029a3dd37d32fc3
hopeson <hopesonogaga@gmail.com>
2024-07-24 13:44:56 +0100

    tailwind setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-300 h-36 w-36 rounded-full flex  justify-center items-center">
+      <div className="bg-green-300 h-20 w-20 rounded-full"></div>
+    </div>
+  );
+}
+
+export default App;

83741c4900ac8228e8739a61273de3902d3f3e15
hopeson <hopesonogaga@gmail.com>
2024-07-24 17:16:26 +0100

    custom colors

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <div className="flex justify-center flex-col items-center gap-4 ">
+      <p className="text-2xl font-bold capitalize text-DarkDesaturatedBlue ">simple, traffice-based pricing</p>
+      <p className="text-xs font-semibold capitalize text-LightGrayishBlue">  Sign-up for our 30-day trial. No credit card required. </p>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        SoftCyan: "hsl(174, 77%, 80%)",
+        StrongCyan: "hsl(174, 86%, 45%)",
+        LightGrayishRed: "hsl(14, 92%, 95%)",
+        LightRed: "hsl(15, 100%, 70%)",
+        PaleBlue: "hsl(226, 100%, 87%)",
+        White: "hsl (0, 0%, 100%)",
+        VeryPaleBlue: "hsl(230, 100%, 99%)",
+        LightGrayishBlue: "hsl(223, 50%, 87%)",
+        GrayishBlue: "hsl(225, 20%, 60%)",
+        DarkDesaturatedBlue: "hsl(227, 35%, 25%)",
+      },
+    },
   },
-  plugins: [],
-}
 
+  plugins: [],
+};
